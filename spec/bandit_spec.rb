033f74769a6f1e3b62d1a74f2767885ff95d5a88
Larry Glenn <yrral.glenn@gmail.com>
2012-06-02 08:10:59 -0400

    Moved kitty.rb and bandit.rb to lib

--- spec/bandit_spec.rb
+++ spec/bandit_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'bandit'
 
 describe Bandits do
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,3 @@
 require 'rspec/core'
 
-$: << File.expand_path(File.dirname(__FILE__) + '../../src/')
-
-require 'bandit'
-
-#require File.expand_path(File.dirname(__FILE__) + '../../sinatra/server.rb')
+$: << File.expand_path(File.dirname(__FILE__) + '../../lib/')
