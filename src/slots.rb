d69e02aed3b48a7f7228429545205676a6d82d10
Larry Glenn <yrral.glenn@gmail.com>
2012-06-02 15:58:51 -0400

    Cleaned up slots.rb

--- src/slots.rb
+++ src/slots.rb
@@ -1,12 +1,18 @@
 require 'bandit'
 require 'kitty'
 
-@chances = { 'a' => 0.02, 'b' => 0.0802 }
+@chances = { 'a' => 0.02, 'b' => 0.03, 'c' => 0.018, 'd' => 0.032 }
 @bandits = Bandits.new
 @rounds = 1000000
 @cost = 25
 @ab_kitty = Kitty.new(@rounds * @cost * @bandits.size)
 @bandit_kitty = Kitty.new(@rounds * @cost * @bandits.size)
+
+def summarize(name)
+    puts "Doing a #{name} test, you've found that #{@bandits.best.name} is the best machine, with a score of #{sprintf("%0.5f",@bandits.best.score)}."
+    puts "The winning bandit was pulled #{@bandits.best.pulls} times out of #{@rounds * @bandits.size} trials, or #{sprintf("%.0f",(@bandits.best.pulls.to_f/(@rounds * @bandits.size).to_f)*100.0)}% of the time."
+    puts
+end
 
 @chances.each do |name,chance|
   @bandits << Bandit.new(name,chance,@cost,@cost*3)
@@ -22,7 +28,7 @@ end
   #end
 end
 
-puts "Doing an A/B test, you've found that the scores are: [#{@bandits.scores}]. And, the test cost you $#{sprintf("%0.2f",@ab_kitty.loss / 100.0)}!"
+summarize "A/B"
 
 @bandits.reset
 
@@ -35,8 +41,4 @@ puts "Doing an A/B test, you've found that the scores are: [#{@bandits.scores}].
   #end
 end
 
-puts "Using a bandit algorithm, you've found that the scores are: [#{@bandits.scores}]. And, the test cost you $#{sprintf("%0.2f",@bandit_kitty.loss / 100.0)}!"
-
-diff = (@ab_kitty.balance - @bandit_kitty.balance)/@ab_kitty.balance.to_f
-
-puts "Your savings were #{sprintf("%0.1f",diff * 100)}%."
+summarize "Bandit"
