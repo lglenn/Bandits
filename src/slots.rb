93a0f034244a460a8bd0dcca6277e59c5bba2ce0
Larry Glenn <yrral.glenn@gmail.com>
2012-06-03 18:37:49 -0400

    slots cleanup

--- src/slots.rb
+++ src/slots.rb
@@ -1,44 +1,56 @@
 require 'bandit'
 require 'kitty'
 
-@chances = { 'a' => 0.02, 'b' => 0.03, 'c' => 0.018, 'd' => 0.032 }
+@chances = { 'a' => 0.02, 'b' => 0.0203 }
 @bandits = Bandits.new
-@rounds = 1000000
-@cost = 25
-@ab_kitty = Kitty.new(@rounds * @cost * @bandits.size)
-@bandit_kitty = Kitty.new(@rounds * @cost * @bandits.size)
+@trials = 8000001
+@print_interval = 100000
 
 def summarize(name)
-    puts "Doing a #{name} test, you've found that #{@bandits.best.name} is the best machine, with a score of #{sprintf("%0.5f",@bandits.best.score)}."
-    puts "The winning bandit was pulled #{@bandits.best.pulls} times out of #{@rounds * @bandits.size} trials, or #{sprintf("%.0f",(@bandits.best.pulls.to_f/(@rounds * @bandits.size).to_f)*100.0)}% of the time."
-    puts
+  puts "Doing a #{name} test, you've found that #{@bandits.best.name} is the best machine, with a score of #{sprintf("%0.5f",@bandits.best.score)}."
+  puts "The winning bandit was pulled #{@bandits.best.pulls} times out of #{@trials} trials, or #{sprintf("%.0f",(@bandits.best.pulls.to_f/@trials.to_f)*100.0)}% of the time."
+  puts
+end
+
+def writestats(i,report)
+  tuple = [i]
+  @bandits.each do |b|
+    tuple << sprintf("%0.6f",b.score)
+    tuple << b.pulls
+  end
+  report.write(tuple.join("\t"))
+  report.write("\n")
 end
 
 @chances.each do |name,chance|
-  @bandits << Bandit.new(name,chance,@cost,@cost*3)
+  @bandits << Bandit.new(name,chance)
 end
 
-@rounds.times do |i|
+report = File.open("./ab.dat","w")
+i = 0
+while i < @trials
   @bandits.each do |bandit|
-    @ab_kitty += bandit.pull
+    bandit.pull
+    i += 1
   end
-  #if i % 1000 == 0
-      #print "#{i}\t"
-      #puts @bandits.scores.collect { |s| sprintf("%0.5f",s) }.join("\t")
-  #end
+  if i % @print_interval == 0
+    writestats(i,report)
+  end
 end
 
 summarize "A/B"
 
 @bandits.reset
+i = 0
+report = File.open("./bandit.dat","w")
 
-(@rounds * @bandits.size).times do |i|
+while i < @trials
   bandit = rand < 0.1 ? @bandits.random : @bandits.best
-  @bandit_kitty += bandit.pull
-  #if i % (@bandits.size * 1000) == 0
-      #print "#{i}\t"
-      #puts @bandits.scores.collect { |s| sprintf("%0.5f",s) }.join("\t")
-  #end
+  bandit.pull
+  i += 1
+  if i % @print_interval == 0
+    writestats(i,report)
+  end
 end
 
 summarize "Bandit"
